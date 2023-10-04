9d78abc88426c1f822fe7683db02d7618deb754f
Manish Tiwari <76205618+techiemanish@users.noreply.github.com>
2023-10-05 00:52:57 +0530

    Optimize way to do Caesars_Cipher rot13 algo

--- Caesars_Cipher.js
+++ Caesars_Cipher.js
@@ -1,36 +1,24 @@
 //Caesars Cipher freecodecamp certification Question
 function rot13(str) {
-  const rot13 = {
-    "A" : "N", "N" : "A",
-    "B" : "O", "O" : "B",
-    "C" : "P", "P" : "C",
-    "D" : "Q", "Q" : "D",
-    "E" : "R", "R" : "E",
-    "F" : "S", "S" : "F",
-    "G" : "T", "T" : "G",
-    "H" : "U", "U" : "H",
-    "I" : "V", "V" : "I",
-    "J" : "W", "W" : "J",
-    "K" : "X", "X" : "K",
-    "L" : "Y", "Y" : "L",
-    "M" : "Z", "Z" : "M",
-    "?" : "?", "." : ".",
-    "!" : "!"
-  }
-  //console.log(Object.keys(rot13).length);
-  let temp = str.split("");
-  //console.log(temp);
+  let arr = str.split("");
+  const alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
   let res = [];
-  for(let i in temp){
-    if(temp[i] != " "){
-      res.push(rot13[temp[i]]);
+  for(let i in arr){
+    if(alphabet.includes(arr[i])){
+      let index = alphabet.indexOf(arr[i]);
+      let newIndex = index+13;
+      if(newIndex > 25){
+        newIndex = newIndex-26;
+      }
+      res.push(alphabet[newIndex]);
     }
     else{
-      res.push(temp[i]);
+      res.push(arr[i]);
     }
   }
   return res.join("");
 }
+
 rot13("SERR YBIR?"); //FREE LOVE?
 rot13("SERR PBQR PNZC"); //FREE CODE CAMP
 rot13("SERR CVMMN!"); //FREE PIZZA!
